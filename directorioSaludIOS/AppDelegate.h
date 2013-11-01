8abc522686a7bb1d2767e96de60b814fda9d1d79
rramirez-innovatie <ronald.ramirez@innovatiecompany.com>
2013-11-01 00:22:23 -0500

    Initial Commit

---
+++ directorioSaludIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  directorioSaludIOS
+//
+//  Created by IngR10 on 11/1/13.
+//  Copyright (c) 2013 iNNovatie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ directorioSaludIOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  directorioSaludIOS
+//
+//  Created by IngR10 on 11/1/13.
+//  Copyright (c) 2013 iNNovatie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
